d11aac477deed6876490f52c5f5d7b1ed3e79a1a
Alex Cheung <alexkk.cheung@gmail.com>
2022-12-20 11:59:09 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+	"encoding/json"
+	"runtime"
+	"math"
+	"log"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+type SSEChannel struct {
+	Clients []chan string
+	Notifier chan string
+}
+
+type PCInfo struct {
+	DateTime string `json:"dateTime"`
+	Os string `json:"os"`
+	MemLoad int `json:"memload"`
+	CpuLoad int `json:"cpuload"`
+}
+
+var sseChannel SSEChannel
+
+func main(){
+	fmt.Println("SSE-GO")
+
+	sseChannel = SSEChannel{
+		Clients: make([]chan string, 0),
+		Notifier: make(chan string),
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	go RunForever()
+
+	go broadcaster(done)
+
+	d := make(chan interface{})
+	defer close(d)
+	defer fmt.Println("Closing channel.")
+
+	http.HandleFunc("/sse", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			http.Error(w, "Connection does not support streaming", http.StatusBadRequest)
+			return
+		}
+
+		sseChan := make(chan string)
+		sseChannel.Clients = append(sseChannel.Clients, sseChan)
+
+		// this goroute reads and streams into the channel
+		d := make(chan interface{})
+		defer close(d)
+		defer fmt.Println("Closing channel.")
+
+		for {
+			select {
+			case <-d:
+				close(sseChan)
+				return
+			case data := <-sseChan:
+				fmt.Printf("data: %v \n\n", data)
+				fmt.Fprintf(w, "data: %v \n\n", data)
+				flusher.Flush()
+			}
+		}
+	})
+
+	http.HandleFunc("/log", logHTTPRequest)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
+
+	fmt.Println("Listening to 5000")
+	http.ListenAndServe(":5000", nil)
+
+}
+
+func logHTTPRequest(w http.ResponseWriter, r *http.Request){
+	buf := new(strings.Builder)
+	if _, err := io.Copy(buf, r.Body); err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+	method := r.Method
+
+	logMsg := fmt.Sprintf("Method %v, Body: %v", method, buf.String())
+	fmt.Println(logMsg)
+	sseChannel.Notifier <- logMsg
+}
+
+func broadcaster(done <-chan interface{}){
+	fmt.Println("Broadcaster Started.")
+	for {
+		select {
+		case <-done:
+			return
+		case data := <-sseChannel.Notifier:
+			for _, channel := range sseChannel.Clients {
+				channel <- data
+			}
+		}
+	}
+}
+
+func showStatistic(ch chan struct{}){
+	time.Sleep(5 * time.Second)
+	t := time.Now()
+
+	ch <- struct{}{}
+	
+	pcInfo := PCInfo{
+		Os: runtime.GOARCH, 
+		CpuLoad: getCpuUsage(),
+		MemLoad: getMemoryUsage(),
+		DateTime: t.Format(time.UnixDate),
+	}
+
+	data,  err := json.Marshal(pcInfo)
+	
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s\n", string(data))
+	sseChannel.Notifier <- string(data)
+
+}
+
+func RunForever(){
+	wait := make(chan struct{})
+	for {
+		go showStatistic(wait)
+		<-wait
+	}
+}
+
+func getCpuUsage()int{
+	percent, err := cpu.Percent(time.Second, false)
+	if err != nil{
+		log.Fatal(err)
+	}
+	return int(math.Ceil(percent[0]))
+}
+
+func getMemoryUsage()int{
+	memory, err := mem.VirtualMemory()
+	if err != nil{
+		log.Fatal(err)
+	}
+	return int(math.Ceil(memory.UsedPercent))
+}
---
+++ my-readme.md
@@ -0,0 +1,20 @@
+
+go mod init
+
+2022-12-16
+
+https://github.com/dipeshdulal/sse-go
+
+https://stackoverflow.com/questions/26559557/how-do-you-serve-a-static-html-file-using-a-go-web-server
+
+2022-12-17
+
+https://stackoverflow.com/questions/55858835/infinite-looping-with-vs-without-time-sleep
+
+2022-12-19
+
+https://www.w3schools.com/js/js_array_methods.asp
+
+https://www.chartjs.org/docs/latest/developers/updates.html
+
+https://www.chartjs.org/docs/latest/samples/line/multi-axis.html
